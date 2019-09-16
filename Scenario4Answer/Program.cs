ff8dc624c58e4e2c08e73a99a5ca7e8be3fedb1f
Ahson Khan <ahkha@microsoft.com>
2019-09-15 20:59:40 -0700

    Update answers based on recent package changes and sync from questions.

--- Scenario4Answer/Program.cs
+++ Scenario4Answer/Program.cs
@@ -6,6 +6,39 @@ namespace Scenario4
 {
     class Program
     {
+        // TODO: Serialize the "Song" object to a JSON representation
+        //       where instead of a number, the property with the Genre
+        //       enum is written as a string. So instead of
+        //
+        //           "Genre": 3
+        //
+        //       it should be
+        //
+        //           "Genre": "PopRock"
+        //
+        private static string Serialize(Song song)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonStringEnumConverter());
+
+            string json = JsonSerializer.Serialize(song, options);
+            return json;
+        }
+
+        // -------------------------------------
+        // The code below SHOULD NOT BE modified
+        // -------------------------------------
+
+        public enum Genre { Classical, Rap, Soul, PopRock, Reggae };
+
+        public class Song
+        {
+            public string Title { get; set; }
+            public string Artist { get; set; }
+            public Genre Genre { get; set; }
+        }
+
+        #region Main
         static void Main(string[] args)
         {
             Song song = new Song()
@@ -17,29 +50,10 @@ namespace Scenario4
 
             string json = Serialize(song);
             Console.WriteLine(json);
-            
+
             Console.WriteLine("Press any key to continue ...");
-            Console.ReadLine();
+            Console.ReadKey();
         }
-
-        // TODO 1) Serialize the "Song" object to a JSON representation where the Genre property is written as a string.
-        // 2) Ensure the Genre property is written as a string. For example, {"Title":"Three Little Birds","Artist":"Bob Marley","Genre":"Reggae"}
-        private static string Serialize(Song song)
-        {
-            JsonSerializerOptions options = new JsonSerializerOptions();
-            options.Converters.Add(new JsonStringEnumConverter());
-
-            string json = JsonSerializer.Serialize(song, options);
-            return json;
-        }
-    }
-
-    public enum Genre { Classical, Rap, Soul, PopRock, Reggae };
-
-    public class Song
-    {
-        public string Title { get; set; }
-        public string Artist { get; set; }
-        public Genre Genre { get; set; }
+        #endregion
     }
 }
